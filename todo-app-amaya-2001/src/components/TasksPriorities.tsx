3e503bd06186a94b21fa77152ae20fd72d676677
Amaya-2001 <112056360+Amaya-2001@users.noreply.github.com>
2023-10-07 16:14:05 +0530

    Update TaskPriorities Component By adding the header and Divider

--- todo-app-amaya-2001/src/components/TasksPriorities.tsx
+++ todo-app-amaya-2001/src/components/TasksPriorities.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import Card from '@mui/material/Card';
+import { styled } from '@mui/system';
+import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
+import Divider from '@mui/material/Divider';
+import Avatar2 from "../Assets/Assets/Avatar-2.svg"
 
 export const TasksPriorities = () => {
+    const TasksPrioritiesStyled = styled(Card)({
+        width: "440px",
+        height: "253px",
+        marginTop: "10px",
+        display: 'flex',
+        marginLeft: '120px',
+        alignItems: 'flext-start',
+        justifyContent: 'space-between',
+        flexDirection: 'column',
+    });
 
     return (
-        <div>
-            Tasks
-        </div>
+        <TasksPrioritiesStyled>
+            <Box sx={{ minWidth: 275 }}>
+                <Card sx={{ width: "440px", height: "2533px" }} variant="outlined"> <Typography sx={{ fontWeight: 'bold', textAlign: 'left', marginLeft: '20px', marginTop: '20px', marginBottom: '20px' }}>Tasks Priorities</Typography>
+                    <Divider />
+
+                </Card>
+            </Box>
+
+        </TasksPrioritiesStyled>
     )
 
 }
