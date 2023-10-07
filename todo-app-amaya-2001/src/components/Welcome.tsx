4f97545a4984c0e035f58c19ecf912d1a325544d
Amaya-2001 <112056360+Amaya-2001@users.noreply.github.com>
2023-10-07 09:15:20 +0530

    Updated Welcome component by adding welcome message

--- todo-app-amaya-2001/src/components/Welcome.tsx
+++ todo-app-amaya-2001/src/components/Welcome.tsx
@@ -1,11 +1,53 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import { styled } from '@mui/system';
+import IconButton from '@mui/material/IconButton';
+import Vector from "../Assets/Assets/Vector.svg";
+import Close from "../Assets/Assets/Close.svg";
 
 export const Welcome = () => {
+    const StyledCard = styled(Card)({
+        width: "1120px",
+        height: "120px",
+        marginLeft: "320px",
+        marginTop: "5px",
+        display: 'flex',
+        alignItems: 'flext-start',
+        justifyContent: 'space-between',
+    });
+
     return (
-        <div>Welcome</div>
-    )
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start', width: '100%' }}>
+            <StyledCard variant="outlined">
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start', width: '100%' }}>
+                    <Box sx={{ alignItems: 'center', marginLeft: '24px', marginTop: "5px" }}>
+                        <Typography variant="h6" gutterBottom sx={{ fontWeight: 'bold' }}>
+                            Welcome Back, John Doe
+                        </Typography>
+                        <Typography variant="caption" display="block" color="#757575" gutterBottom>
+                            The end of the year is coming. Are you planning your performance interviews? You can do this super efficiently with Acmy.
+                        </Typography>
+                        <Typography variant="caption" display="block" gutterBottom>
+                            <Link href="#" color="#BC006D">
+                                Look here for more information
+                            </Link>
+                        </Typography>
+                    </Box>
+                    <Box sx={{ display: 'flex', alignItems: 'center', marginRight: '24px' }}>
+                        <img src={Vector} alt="Vector" />
+                        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', marginTop: '5px', }}>
+                            <IconButton size="large" aria-label="display more actions" edge="end"  >
+                                <img src={Close} alt="Close" />
+                            </IconButton>
+                        </Box>
+                    </Box>
+                </Box>
+            </StyledCard>
+        </Box>
+    );
 }
-
-
 
 export default Welcome;
