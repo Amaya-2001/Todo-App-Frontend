c39ee8226827c0a35487a643c486ca8f52a649d7
Amaya-2001 <112056360+Amaya-2001@users.noreply.github.com>
2023-10-07 22:46:36 +0530

    Create function to mark done tasks

--- todo-app-amaya-2001/src/components/Tasks.tsx
+++ todo-app-amaya-2001/src/components/Tasks.tsx
@@ -9,15 +9,18 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 import { ITask } from '../../types/tasks';
+import PriorityHigh from "../Assets/Assets/Priority-High.svg"
+import PriorityMedium from "../Assets/Assets/Priority-Medium.svg"
+import PriorityLow from '../Assets/Assets/Priority-Low.svg'
 
 //styled component
 const TasksCard = styled(Card)({
     width: "656px",
-    height: "632px",
+    height: "656px",
     marginTop: "54px",
     display: 'flex',
     marginLeft: '54px',
@@ -25,6 +28,20 @@ const TasksCard = styled(Card)({
     justifyContent: 'space-between',
     flexDirection: 'column',
 });
+const MarkAsDoneBtnStyle = {
+    width: '100px',
+    height: '15px',
+    marginTop: '0',  // Adjusted margin for better positioning
+    borderRadius: '15px',  // Adjusted border radius for a smaller button
+    textTransform: 'capitalize',
+    color: '#BC006D',
+    fontSize: '10px',  // Font
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between'
+
+
+};
 
 export const Tasks = () => {
     const [tasks, setTasks] = useState<ITask[]>([]);
@@ -48,6 +65,17 @@ export const Tasks = () => {
         settablePage(newPage);
     };
 
+    //handle Mark as done tasks
+    const handleDoneTasks = (taskId: string) => {
+        const updatedTasks = tasks.map((task) =>
+            task.id === taskId ? { ...task, completed: true } : task
+        );
+        setTasks(updatedTasks);
+
+
+
+    }
+
     //logic - to display number of tasks per-page
     const startIndex = (tablePage - 1) * tasksPerTablePage;
     const endIndex = startIndex + tasksPerTablePage;
@@ -58,7 +86,7 @@ export const Tasks = () => {
             <Box sx={{ minWidth: 275, flexGrow: 1 }}>
                 <Card sx={{ width: '100%', height: '100%' }} variant="outlined">
                     <Box>
-                        <TableContainer component={Paper} flex-grow={1}>
+                        <TableContainer component={Paper} flex-gro={1}>
                             <Table aria-label="simple table">
                                 <TableHead>
                                     <TableRow>
@@ -76,10 +104,17 @@ export const Tasks = () => {
                                             sx={{ '&:last-child td, &:last-child th': { border: 0 }, fontSize: '10px', }}
                                         >
                                             <TableCell component="th" scope="row" sx={{ fontSize: '10px' }}>
-
-                                                {task.priority}
+                                                {task.priority == "HIGH" && <img src={PriorityHigh} alt="PriorityHigh" />}
+                                                {task.priority == "MEDIUM" && <img src={PriorityMedium} alt="PriorityMedium" />}
+                                                {task.priority == "LOW" && <img src={PriorityLow} alt="LowForBlue" />}
                                             </TableCell>
-                                            <TableCell align="right" sx={{ fontSize: '10px' }}>{task.todo}</TableCell>
+                                            <TableCell align="right" sx={{ fontSize: '10px', textAlign: 'left' }}>{task.todo}
+                                                {task.completed ? (
+                                                    ''
+                                                ) : (
+                                                    <Button sx={MarkAsDoneBtnStyle} onClick={() => handleDoneTasks(task.id)} >Mark as done</Button>
+                                                )}
+                                            </TableCell>
                                             <TableCell align="center">
                                                 <div style={{
                                                     fontSize: '10px', color: task.completed ? '#219653' : '#F2C94C',
@@ -104,8 +139,8 @@ export const Tasks = () => {
                     <Box sx={{
                         display: 'flex',
                         justifyContent: 'center',
-                        marginTop: '20px',
-                        marginBottom: '20px',
+                        marginTop: '16px',
+                        marginBottom: '16px',
                     }}>
                         <Stack spacing={2}>
                             <Pagination count={3} page={tablePage} onChange={handlePage} variant="outlined" shape="rounded" />
