7022d2ebaf713e0d96bfe61e0bf044e2eaaa742f
Amaya-2001 <112056360+Amaya-2001@users.noreply.github.com>
2023-10-07 07:51:51 +0530

    Created Welcome component

--- todo-app-amaya-2001/src/components/Dashboard.tsx
+++ todo-app-amaya-2001/src/components/Dashboard.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navigation from './Navigation';
+import Welcome from './Welcome';
 
-export const Dashboard =() =>{
-    return(
-        <div>
-          <Navigation/>
-        </div>
-    )
+export const Dashboard = () => {
+  return (
+    <div>
+      <Navigation />
+      <Welcome />
+    </div>
+  )
 }
 
 export default Dashboard;
